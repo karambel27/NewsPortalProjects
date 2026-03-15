03af698bcb7374fbbb338671b88e5b1d59cb0b61
Karambel <panovdaniil2015@list.ru>
2026-03-15 20:26:47 +1000

    Сделай функцию update_rating

--- NewsPortal/News/models.py
+++ NewsPortal/News/models.py
@@ -6,6 +6,20 @@ class Author(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     rating = models.IntegerField(default=0)
 
+    def update_rating(self):
+        """
+        Обновляет рейтинг автора исходя из трех составляющих:
+        1. Суммы оценок статей, умноженная на 3.
+        2. Суммы оценок собственных комментариев.
+        3. Суммы оценок комментариев под своими статьями.
+        """
+        summ_article = sum([posts.rating for posts in Post.objects.filter(author=self, type='article')])*3
+        summ_comments = sum([comments.rating for comments in Comment.objects.filter(user=self.user)])
+        summ_author = sum([comments.rating for comments in Comment.objects.filter(post__author=self, post__type='article')])
+        self.rating = summ_article+summ_comments+summ_author
+        self.save()
+
+
 
 
 class Category(models.Model):
@@ -25,7 +39,7 @@ class Post(models.Model):
     author = models.ForeignKey(Author, on_delete=models.CASCADE)
     type = models.CharField(max_length=7, choices=POST_TYPES)
     created_at = models.DateTimeField(auto_now_add=True)
-    categories = models.ManyToManyField(Category, through='PostCategory',
+    categories = models.ManyToManyField(Category, through='PostCategory', blank=True,
                                         related_name='cat')
     title = models.CharField(max_length=255)
     content = models.TextField()
